044281ed146f686ea68547ead25f43d8c218dbde
ChatGavin <0xduer@gmail.com>
2025-03-21 00:59:00 +0800

    refactor: 更新 package.json 中的脚本命令，简化文档构建流程；修改部署工作流以使用新命令；更新文档首页内容和配置，添加新的 VitePress 配置文件和图标资源。

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,56 @@
+export default {
+  title: "Chat with Gavin",
+  description: "基于 Claude 的智能助手",
+  head: [
+    ["link", { rel: "icon", href: "img/logo/vitepress.svg" }],
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        href: "img/logo/vitepress.svg",
+        sizes: "180x180",
+      },
+    ],
+  ],
+  themeConfig: {
+    logo: "img/logo/vitepress.svg",
+    lastUpdated: true,
+    lastUpdatedText: "最后更新于",
+    docFooter: {
+      prev: "上一页",
+      next: "下一页",
+    },
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "指南", link: "guide/" },
+      {
+        text: "更新日志",
+        link: "https://github.com/gavin-gong/ChatGavin/releases",
+      },
+    ],
+    socialLinks: [
+      {
+        icon: "github",
+        link: "https://github.com/gavin-gong/ChatGavin",
+      },
+    ],
+    footer: {
+      message: "Released under the MIT License.",
+      copyright: "Copyright © 2024-present Gavin Lin",
+    },
+    outlineTitle: "目录",
+    sidebar: {
+      "/guide/": [
+        {
+          text: "指南",
+          link: "/guide/",
+        },
+      ],
+    },
+    algolia: {
+      appId: "BY3PPJ4B38",
+      apiKey: "58414decdc73fcf81475dfab24620d22",
+      indexName: "chatgavin",
+    },
+  },
+};
--- docs/index.md
+++ docs/index.md
@@ -4,8 +4,11 @@ layout: home
 
 hero:
   name: "ChatGavin"
-  text: "ChatGavin"
-  tagline: My great project tagline
+  text: "Chat with Gavin"
+  tagline: 我的笔记, 不是存储知识, 而是描摹一个真实而不断变化的自己
+  image:
+    src: /img/logo/vitepress.svg
+    alt: vitepress
   actions:
     - theme: brand
       text: Markdown Examples
@@ -15,11 +18,10 @@ hero:
       link: /api-examples
 
 features:
-  - title: Feature A
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: AI 时代还有必要记笔记吗?
+    details: 不仅有必要甚至更重要, 但不应该是已知信息的重复记录, 而是要记录下自己的偏见与无知, 记录下自己费心思和把手弄脏的实践过程, 记录那些无法被大模型预测的独属于自己的心情与感悟, 只有这样才知道什么样的问题对自己真正重要
   - title: Feature B
     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
   - title: Feature C
     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
 ---
-
